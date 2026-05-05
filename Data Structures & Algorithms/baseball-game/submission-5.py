7f18acd1d36a91a7171426a64416639f2c0d4011
Mohamed Akash <71470845+MohamedAkash@users.noreply.github.com>
2026-05-05 15:10:28 -0400

    Add: baseball-game - submission-5

---
+++ Data Structures & Algorithms/baseball-game/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stk = []
+        for i in operations:
+            if i == '+':
+                temp = stk[-1] + stk[-2]
+                stk.append(temp)
+            elif i == "C":
+                stk.pop()
+            elif i == 'D':
+                stk.append(stk[-1]*2)
+            else:
+                stk.append(int(i))
+            
+        return sum(stk)
+
